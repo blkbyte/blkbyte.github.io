168bca2dbd4e9ad5521e0c9f26e008598bc41a48
SysTimur <timur.mutaliev111@gmail.com>
2026-03-24 14:30:25 +0100

    feat: add initial terminal commands

--- js/commandHandlers.js
+++ js/commandHandlers.js
@@ -2,15 +2,20 @@ const input = document.getElementById('input');
 const output = document.getElementById('output');
 
 input.addEventListener('keydown', function(event) {
-    if (event.key  === 'Enter') {
+    if (event.key === 'Enter') {
         const command = input.value.trim().toLowerCase();
         input.value = "";
         output.innerHTML = ""; // очищаем поле
 
         if (command === 'help') {
-            output.innerHTML += 'help - показать список доступных команд<br>';
-        } else {
+            output.innerHTML += 'help - показать список доступных команд<br>about - обо мне<br>clear - очистка экрана';
+        } else if (command === 'about') { // обо мне
+            output.innerHTML += 'Сейчас я изучаю fullstack-разработку, но особенно интересуюсь низкоуровневым программированием (C, системное программирование) и тем, как устроены операционные системы.<br>';
+        } else if (command === 'clear') { // очистка экрана
+            output.innerHTML += '';
+        } 
+        else {
             output.innerHTML += 'command not found<br>';
         }
     }
-})
+});
